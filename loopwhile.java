9d3f8fae78290c981d8f3deec81c7e7db574dfef
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-08 14:22:07 +0400

    Add files via upload

---
+++ loopwhile.java
@@ -0,0 +1,20 @@
+public class loopwhile{
+	public static void main(String[] arg){
+		int i=1;
+		while (i<=10){
+			if (i%2!=0){
+				if (i==5){
+					i++;
+					continue;
+			
+				}
+				System.out.println(i);
+			}
+			i++;	
+			
+		}
+
+
+	}
+
+}
